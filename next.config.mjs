15f048c573a66fcac8aa8cf8a7980923d59b5ec7
FlakoBIG <Collado_81@hotmail.cl>
2024-07-24 21:53:54 -0400

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out', // Este es el directorio que se generará al exportar el proyecto
+  async exportPathMap(defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/menu': { page: '/menu' },
+      '/Modificar_recluta': { page: '/Modificar_recluta' },
+      '/Modificar_usuario': { page: '/Modificar_usuario' },
+      '/Registrar_reclutas': { page: '/Registrar_reclutas' },
+      '/Registrar_usuario': { page: '/Registrar_usuario' },
+      '/Visualisar_reclutas': { page: '/Visualisar_reclutas' },
+      '/Visualisar_usuarios': { page: '/Visualisar_usuarios' },
+    };
+  },
 };
 
 export default nextConfig;
